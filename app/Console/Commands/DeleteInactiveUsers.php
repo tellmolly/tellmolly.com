840264a10de6b639b0336b13bf46b09b1b98760d
Alexander Glück <github@alexanderglueck.at>
2022-12-29 14:30:11 +0100

    Add command to delete inactive users

---
+++ app/Console/Commands/DeleteInactiveUsers.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use Illuminate\Console\Command;
+
+class DeleteInactiveUsers extends Command
+{
+    protected $signature = 'tm:delete-inactive-users';
+
+    protected $description = 'Delete users that have a) no entries and no activity in the last 30+180 days or b) no activity for the last 180+30 days';
+
+    public function handle(): int
+    {
+        $inactiveUsersQuery = User::query()
+            ->where('last_login_at', '<', now()->subDays(30 + 180)->format('Y-m-d') . ' 00:00:00');
+
+        $affected = $inactiveUsersQuery->count();
+
+        if ($affected === 0) {
+            $this->info('No inactive users detected');
+
+            return Command::SUCCESS;
+        }
+
+        $this->info('Deleting ' . $affected . ' inactive users');
+
+        if ( ! $inactiveUsersQuery->delete()) {
+            $this->error('Failed deleting ' . $affected . ' inactive users');
+
+            return Command::FAILURE;
+        }
+
+        $this->info('Deleted ' . $affected . ' inactive users');
+
+        return Command::SUCCESS;
+    }
+}
