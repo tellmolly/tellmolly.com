301515e5877bc5434920b073778a5ceb511a6abc
Alexander Glück <github@alexanderglueck.at>
2022-12-29 14:29:22 +0100

    Update last_login_at on login

---
+++ app/Listeners/UpdateLastLoginAtTimestamp.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Models\User;
+use Illuminate\Auth\Events\Login;
+use Illuminate\Auth\Events\Registered;
+
+class UpdateLastLoginAtTimestamp
+{
+
+    public function handle(Registered|Login $event)
+    {
+        if ( ! $event->user instanceof User) {
+            return;
+        }
+
+        $event->user->last_login_at = now();
+        $event->user->update();
+    }
+}
--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Listeners\UpdateLastLoginAtTimestamp;
+use Illuminate\Auth\Events\Login;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -17,7 +19,11 @@ class EventServiceProvider extends ServiceProvider
     protected $listen = [
         Registered::class => [
             SendEmailVerificationNotification::class,
+            UpdateLastLoginAtTimestamp::class
         ],
+        Login::class => [
+            UpdateLastLoginAtTimestamp::class
+        ]
     ];
 
     /**
