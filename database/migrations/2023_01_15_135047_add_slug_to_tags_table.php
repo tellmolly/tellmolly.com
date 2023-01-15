bcc9bf1caa5497af37881b7659672c633b0eb21c
Alexander Glück <github@alexanderglueck.at>
2023-01-15 15:35:09 +0100

    Add slug to tags table

---
+++ database/migrations/2023_01_15_135047_add_slug_to_tags_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('tags', function (Blueprint $table) {
+            $table->string('slug')->default(null)->nullable();
+
+            $table->unique(['slug', 'user_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('tags', function (Blueprint $table) {
+            $table->dropIndex('tags_slug_user_id_unique');
+
+            $table->dropColumn('slug');
+        });
+    }
+};
