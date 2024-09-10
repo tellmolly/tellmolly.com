278352290f6c3fbf3885c7212a6fc49430bc4853
Alexander Glück <github@alexanderglueck.at>
2024-09-10 19:30:30 +0200

    Fix password reset not working

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -11,7 +11,7 @@
                 <div class="card-header">{{ __('Reset Password') }}</div>
 
                 <div class="card-body">
-                    <form method="POST" action="{{ route('password.update') }}">
+                    <form method="POST" action="{{ route('password.store') }}">
                         @csrf
 
                         <input type="hidden" name="token" value="{{ $token }}">
