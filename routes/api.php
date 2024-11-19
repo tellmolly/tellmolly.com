9f0196427ced446e81a50cb4d222a370a8f99101
Alexander Glück <github@alexanderglueck.at>
2024-11-19 22:46:30 +0100

    Add prefix to API resource routes

--- routes/api.php
+++ routes/api.php
@@ -23,10 +23,10 @@ Route::middleware('guest')->group(function () {
 });
 
 Route::group(['middleware' => ['auth:sanctum']], function () {
-    Route::resource('tags', \App\Http\Controllers\API\TagController::class)->except(['show', 'create']);
+    Route::resource('tags', \App\Http\Controllers\API\TagController::class, ['as' => 'api'])->except(['show', 'create']);
     Route::get('days/calendar', [\App\Http\Controllers\API\QuickDayController::class, 'index']);
     Route::post('days/exists', [\App\Http\Controllers\API\QuickDayController::class, 'exists']);
-    Route::resource('days', \App\Http\Controllers\API\DayController::class)->except(['show']);
+    Route::resource('days', \App\Http\Controllers\API\DayController::class, ['as' => 'api'])->except(['show']);
     Route::post('days/jump', [\App\Http\Controllers\API\DayController::class, 'jump']);
     Route::get('days/{day}/previous', [\App\Http\Controllers\API\DayController::class, 'previous']);
     Route::get('days/{day}/next', [\App\Http\Controllers\API\DayController::class, 'next']);
@@ -37,10 +37,10 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::patch('/profile', [\App\Http\Controllers\API\ProfileController::class, 'update']);
     Route::delete('/profile', [\App\Http\Controllers\API\ProfileController::class, 'destroy']);
 
-    Route::get('year/months/{year?}', [\App\Http\Controllers\API\YearMonthController::class, 'index'])->name('year-month.index');
-    Route::get('year/{year?}', [\App\Http\Controllers\API\YearController::class, 'index'])->name('year.index');
+    Route::get('year/months/{year?}', [\App\Http\Controllers\API\YearMonthController::class, 'index']);
+    Route::get('year/{year?}', [\App\Http\Controllers\API\YearController::class, 'index']);
 
     Route::get('year-in-review', [\App\Http\Controllers\API\YearInReviewController::class, 'show']);
 
-    Route::post('export', \App\Http\Controllers\ExportController::class)->name('export');
+    Route::post('export', \App\Http\Controllers\ExportController::class);
 });
