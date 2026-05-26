6995d5f49dc91193efe44fad51689dea00158f0d
Alexander Glück <github@alexanderglueck.at>
2026-05-26 20:37:54 +0200

    Silence css deprecations

--- vite.config.js
+++ vite.config.js
@@ -11,4 +11,12 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    css: {
+        preprocessorOptions: {
+            scss: {
+                quietDeps: true,
+                silenceDeprecations: ['import', 'global-builtin', 'color-functions'],
+            },
+        },
+    },
 });
